e090776eba0cc81d4b66b1ec21e1aaa837daa873
Mark Southee <mark.southee@gmail.com>
2014-07-05 15:30:23 +0100

    Completed html and PHP changes

--- HTML/contact.php
+++ HTML/contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'yourmail@example.com';
-$siteTitle = 'YourSiteTitle';
+$emailTo = 'mark@southee.co.uk';
+$siteTitle = 'From southee.co.uk';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
